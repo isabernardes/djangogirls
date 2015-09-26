f901b7138699d2f77cc3ce90356a05170cbc605d
isabernardes <isabelbernardes90@gmail.com>
2015-09-26 16:59:34 +0200

    initial commit, showing basic page with form and saving

---
+++ mysite/models.py
@@ -0,0 +1,8 @@
+# Models
+from django.db import models
+from django.utils import timezone
+
+class Post(models.Model):
+	title = models.CharField(max_length = 200)
+	content = models.TextField()
+	created = models.DateTimeField(default=timezone.now) 
